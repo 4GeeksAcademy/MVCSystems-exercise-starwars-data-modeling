999bce74884c37e2fc3bc3579cb6149421724377
MVCSystems <carlosmerav@gmail.com>
2024-08-27 23:54:10 -0500

    Add

--- src/models.py
+++ src/models.py
@@ -31,11 +31,9 @@
 # ## Draw from SQLAlchemy base
 # render_er(Base, 'diagram.png')
 
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from eralchemy2 import render_er
 
 Base = declarative_base()
@@ -46,9 +44,9 @@ class User(Base):
     name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
-    favorite_character = relationship("Favorite_Character", backref = "user", lazy=True)
-    favorite_planet = relationship("Favorite_Planet", backref = "user", lazy=True)
-    favorite_vehicle = relationship("Favorite_Vehicle", backref = "user", lazy=True)
+    favorite_characters = relationship("FavoriteCharacter", backref="user", lazy=True)
+    favorite_planets = relationship("FavoritePlanet", backref="user", lazy=True)
+    favorite_vehicles = relationship("FavoriteVehicle", backref="user", lazy=True)
 
 class Character(Base):
     __tablename__ = 'character'
@@ -60,7 +58,7 @@ class Character(Base):
     skin_color = Column(String(50), nullable=False)
     eye_color = Column(String(50), nullable=False)
     gender = Column(String(50), nullable=False)
-    favorite_character = relationship("Favorite_Character", backref = "character", lazy=True)
+    favorite_characters = relationship("FavoriteCharacter", backref="character", lazy=True)
 
 class Planet(Base):
     __tablename__ = 'planet'
@@ -74,9 +72,9 @@ class Planet(Base):
     rotation_period = Column(Integer, nullable=False)
     surface_water = Column(Integer, nullable=False)
     terrain = Column(String(50), nullable=False)
-    favorite_planet = relationship("Favorite_Planet", backref = "planet", lazy=True)
+    favorite_planets = relationship("FavoritePlanet", backref="planet", lazy=True)
 
-class Vehicles(Base):
+class Vehicle(Base):
     __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -87,29 +85,27 @@ class Vehicles(Base):
     vehicle_class = Column(String(50), nullable=False)
     manufacturer = Column(String(50), nullable=False)
     passengers = Column(Integer, nullable=False)
-    favorite_vehicle = relationship("Favorite_Vehicle", backref = "vehicle", lazy=True)
+    favorite_vehicles = relationship("FavoriteVehicle", backref="vehicle", lazy=True)
 
-class Favorite_Character(Base):
+class FavoriteEntity(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+class FavoriteCharacter(FavoriteEntity):
     __tablename__ = 'favorite_character'
-    id = Column(Integer, primary_key=True)
     character_id = Column(Integer, ForeignKey("character.id"), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
-class Favorite_Planet(Base):
+class FavoritePlanet(FavoriteEntity):
     __tablename__ = 'favorite_planet'
-    id = Column(Integer, primary_key=True)
     planet_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
-class Favorite_Vehicle(Base):
+class FavoriteVehicle(FavoriteEntity):
     __tablename__ = 'favorite_vehicle'
-    id = Column(Integer, primary_key=True)
     vehicle_id = Column(Integer, ForeignKey("vehicle.id"), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
     def to_dict(self):
         return {}
-  
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')

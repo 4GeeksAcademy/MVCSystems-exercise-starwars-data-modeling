9240188a09bd629d31a60f0c03c136c65ef0fab9
MVCSystems <carlosmerav@gmail.com>
2024-08-28 00:06:48 -0500

    Add

--- src/models.py
+++ src/models.py
@@ -32,8 +32,8 @@
 # render_er(Base, 'diagram.png')
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship , declarative_base
+from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
